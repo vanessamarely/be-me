f2f752bb0783e788eadac1f8862af6c4b461063e
Luisa Ramirez C <luisaf.ramirez.cardona11@gmail.com>
2023-09-22 15:49:23 -0500

    Tarea DarkMode en React con Tailwind y practica Conceptos Basicos

--- projects/InRoute/TailwindDarkMode/DarkMode/tailwind.config.js
+++ projects/InRoute/TailwindDarkMode/DarkMode/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-  darkMode: "class",
-  plugins: [require("tailwindcss-animated")],
 };
