c865a2e9263d131d1d909aedb5598edc1cfd1feb
Mariana <mariarroyave05@gmail.com>
2023-10-02 20:50:48 -0500

    adding examples

--- projects/the-geeks/hooks/src/App.jsx
+++ projects/the-geeks/hooks/src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 import "./index.css";
 
 const App = () => {
-  const [contador, cambiarContador] = useState(0); //Estableciendo el estado a un valor de 0
-  const nombre = useRef("Mariana"); //Variable que no se va a perder entre cambios de estado
-  const cuentaRenderizado = useRef(0); //Cuantas veces se ha renderizado la página
+  const [contador, cambiarContador] = useState(0); 
+  const nombre = useRef("Mariana");
+  const cuentaRenderizado = useRef(0);
 
   useEffect(() => {
     if (cuentaRenderizado.current === 0) {
@@ -13,9 +13,8 @@ const App = () => {
       return;
     }
 
-    console.log(`El contador se actualizó. El nuevo número es ${contador}`); //No es lo correcto
-    //Importamos use effect
-  }, [contador]); //dependencia. El mensaje cambia cuando cambia el contador
+    console.log(`El contador se actualizó. El nuevo número es ${contador}`);
+  }, [contador]);
 
   const cambiarNombre = () => {
     nombre.current = "Juangui";
@@ -23,56 +22,67 @@ const App = () => {
   };
 
   return (
-    //renderizando
-    <div className="bg-rose-300 w-scren h-screen">
-      <div>
-      <h1>Clase de Hooks</h1>
-      <p>
-        Los hooks en React son funciones especiales que te permiten "enganchar"
-        o acceder a características de React en componentes de función. Antes de
-        la introducción de hooks, las características de React, como el estado o
-        el ciclo de vida, solo estaban disponibles en componentes de clase. Los
-        hooks permiten que los componentes de función tengan estas
-        características, lo que hace que el código sea más reutilizable y fácil
-        de entender.
-      </p>
+    <div className="bg-rose-300 min-h-screen items-center  flex justify-center">
+      <div className="bg-white rounded-xl p-5">
+        <h1 className="text-3xl font-bold mb-4">Clase de Hooks</h1>
+        <p>
+          Los hooks en React son funciones especiales que te permiten "enganchar"
+          o acceder a características de React en componentes de función. Antes de
+          la introducción de hooks, las características de React, como el estado o
+          el ciclo de vida, solo estaban disponibles en componentes de clase. Los
+          hooks permiten que los componentes de función tengan estas
+          características, lo que hace que el código sea más reutilizable y fácil
+          de entender.
+        </p>
 
-      <h2>Algunos de los hooks más comunes son:</h2>
-      <p>
-        useState: Permite agregar estado a componentes de función, lo que
-        permite que los componentes gestionen su propio estado interno.
-      </p>
-      <p>
-        useEffect: Se utiliza para realizar efectos secundarios en componentes
-        de función, como hacer llamadas a API, suscripciones a eventos o
-        manipulación del DOM, después de que el componente se renderice.
-      </p>
+       <strong><h2 className="text-1xl mt-4">Algunos de los hooks más comunes son:</h2></strong> 
+        <br />
+        <ul className="list-disc pl-6">
+          <li>
+            <p className="mb-2">
+              <strong>useState:</strong> Permite agregar estado a componentes de función, lo
+              que permite que los componentes gestionen su propio estado interno.
+            </p>
+          </li>
+          <li>
+            <p className="mb-2">
+              <strong>useEffect:</strong> Se utiliza para realizar efectos secundarios en componentes
+              de función, como hacer llamadas a API, suscripciones a eventos o
+              manipulación del DOM, después de que el componente se renderice.
+            </p>
+          </li>
+          <li>
+            <p className="mb-2">
+              <strong>useRef:</strong> Permite crear y acceder a referencias a elementos DOM en componentes 
+              de función, lo que es útil para realizar tareas como enfoque de elementos y medición de elementos. 
+              Cuando usamos useRef, nos proporciona algo parecido a un contenedor que puede almacenar un valor. 
+              Este contenedor tiene una propiedad especial llamada .current.
+            </p>
+          </li>
+        </ul>
+
+        <div className="mt-6">
+          <p className="mb-2">Contador: {contador}</p>
+          <button
+            className="bg-rose-300 text-white px-4 py-2 rounded"
+            onClick={() => cambiarContador(contador + 1)}
+          >
+            Añadir 1
+          </button>
+        </div>
+
+        <div className="mt-4">
+          <h1 className="mb-2">Nombre: {nombre.current}</h1>
+          <button
+            className="bg-rose-300 text-white px-4 py-2 rounded"
+            onClick={cambiarNombre}
+          >
+            Cambiar Nombre
+          </button>
+        </div>
       </div>
-      <div className="py-8 px-6 bg-white rounded-xl max-w-sm mt-3">
-        <p>Contador: {contador}</p>
-        <button onClick={() => cambiarContador(contador + 1)}>Añadir 1</button>
-        <h1>Nombre: {nombre.current}</h1>
-        <button onClick={cambiarNombre}>Cambiar Nombre</button>
-      </div>
-
-
     </div>
   );
 };
-/*function App() {
-  return (
-    <>
-      <main className="bg-rose-300 w-scren h-screen">
-        <h1>Clase de Hooks</h1>
-        <p>Los hooks en React son funciones especiales que te permiten "enganchar" o acceder a características de React en componentes de función. Antes de la introducción de hooks, las características de React, como el estado o el ciclo de vida, solo estaban disponibles en componentes de clase. Los hooks permiten que los componentes de función tengan estas características, lo que hace que el código sea más reutilizable y fácil de entender.</p>
 
-        <h2>Algunos de los hooks más comunes son:</h2>
-        <p>useState: Permite agregar estado a componentes de función, lo que permite que los componentes gestionen su propio estado interno.</p>
-        <p>useEffect: Se utiliza para realizar efectos secundarios en componentes de función, como hacer llamadas a API, suscripciones a eventos o manipulación del DOM, después de que el componente se renderice.</p>
-        <p>useRef: Permite crear y acceder a referencias a elementos DOM en componentes de función, lo que es útil para realizar tareas como enfoque de elementos y medición de elementos.</p>
-      </main>
-    </>
-  );
-}
-*/
 export default App;
